33fda941a88390ded981e9aa88468cf6c20fad6d
rohit <rohit.jagtap10@gmail.com>
2016-06-15 19:14:51 +0530

    ROOM-2 : detected charaters and digits.

--- module1/sample1.cpp
+++ module1/sample1.cpp
@@ -23,41 +23,19 @@ using namespace std;
 using namespace cv;
 
 RNG rng(12345);
-
+void sort(vector<Rect> &boundRectCrop);
+int count_crop = 0;
 int main(int argc, char* argv[])
 {
-    char *outText;
-char imageToSave[50];
-char imageToSave_crop[50];
-    tesseract::TessBaseAPI *api = new tesseract::TessBaseAPI();
-api->SetVariable("tessedit_char_whitelist", "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz");
-api->SetVariable("classify_font_name", "Arial.ttf");
+	char *outText;
+	char imageToSave[50];
+	char imageToSave_crop[50];
 
-/*
-
-Font names
-
-Andale_Mono.ttf
-Arial_Black.ttf
-Arial_Bold.ttf
-Arial.ttf
-buttf
-Comic_Sans_MS_Bold.ttf
-Comic_Sans_MS.ttf
-Courier_New_Bold.ttf
-Courier_New.ttf
-Georgia_Bold.ttf
-Georgia.ttf
-Gottf
-Impact.ttf
-Times_New_Roman_Bold.ttf
-Times_New_Roman.ttf
-Trebuchet_MS_Bold.ttf
-Trebuchet_MS.ttf
-ttf
-Verdana_Bold.ttf
-Verdana.ttf
-*/
+	string folderName = "crop";
+	string folderRemoveCommand = "rm -rf ../" + folderName;
+	system(folderRemoveCommand.c_str());	//Remove the folder if already present
+	string folderCreateCommand = "mkdir ../" + folderName;
+	system(folderCreateCommand.c_str());	//Create the folder if not present.
 
 
 
@@ -65,269 +43,354 @@ Verdana.ttf
 
 
 
+	tesseract::TessBaseAPI *api = new tesseract::TessBaseAPI();
+	api->SetVariable("tessedit_char_whitelist", "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz");
+	api->SetVariable("classify_font_name", "Arial.ttf");
+
+	/*
+
+	   Font names
+
+	   Andale_Mono.ttf
+	   Arial_Black.ttf
+	   Arial_Bold.ttf
+	   Arial.ttf
+	   buttf
+	   Comic_Sans_MS_Bold.ttf
+	   Comic_Sans_MS.ttf
+	   Courier_New_Bold.ttf
+	   Courier_New.ttf
+	   Georgia_Bold.ttf
+	   Georgia.ttf
+	   Gottf
+	   Impact.ttf
+	   Times_New_Roman_Bold.ttf
+	   Times_New_Roman.ttf
+	   Trebuchet_MS_Bold.ttf
+	   Trebuchet_MS.ttf
+	   ttf
+	   Verdana_Bold.ttf
+	   Verdana.ttf
+	 */
 
 
 
 
 
-api->SetVariable("segment_penalty_garbage", "0");
-api->SetVariable("segment_penalty_dict_nonword", "0");
-api->SetVariable("segment_penalty_dict_frequent_word", "0");
-api->SetVariable("segment_penalty_dict_case_ok", "0");
-api->SetVariable("segment_penalty_dict_case_bad", "0");
 
 
- api->Init("", "eng", tesseract::OEM_DEFAULT);
 
 
-if(!api->SetVariable("tessedit_enable_doc_dict", "0"))
-{
-    cout << "Unable to enable dictionary" << endl;
+
+
+
+	api->SetVariable("segment_penalty_garbage", "0");
+	api->SetVariable("segment_penalty_dict_nonword", "0");
+	api->SetVariable("segment_penalty_dict_frequent_word", "0");
+	api->SetVariable("segment_penalty_dict_case_ok", "0");
+	api->SetVariable("segment_penalty_dict_case_bad", "0");
+
+
+	api->Init("", "eng", tesseract::OEM_DEFAULT);
+
+
+	if(!api->SetVariable("tessedit_enable_doc_dict", "0"))
+	{
+		cout << "Unable to enable dictionary" << endl;
+	}
+
+	//api->SetVariable("segment_penalty_garbage", "0");
+	//api->SetVariable("segment_penalty_dict_nonword", "0");
+	//api->SetVariable("segment_penalty_dict_frequent_word", "0");
+	//api->SetVariable("segment_penalty_dict_case_ok", "0");
+	//api->SetVariable("segment_penalty_dict_case_bad", "0");
+	// api->SetPageSegMode(static_cast<tesseract::PageSegMode>(7));
+	// api->SetOutputName("out");
+
+	// Initialize tesseract-ocr with English, without specifying tessdata path
+	//    if (api->Init(NULL, "eng")) {
+	//        fprintf(stderr, "Could not initialize tesseract.\n");
+	//        exit(1);
+	//    }
+
+
+
+	// Open input image with leptonica library
+	//    Pix *image = pixRead("../../helloworld.jpg");
+	//
+	// api->SetPageSegMode(static_cast<tesseract::PageSegMode>(7));
+	// api->SetOutputName("out");
+
+	Mat gray, imgThresh;
+	Mat img_src = imread(argv[1]);
+	float conf = 0;
+	float conf_avg = 0;
+	//do{
+	cvtColor(img_src,gray,CV_BGR2GRAY);
+	imshow("gray",gray);
+	cv::GaussianBlur(gray, gray, cv::Size(3, 3), 0);
+	imshow("blur",gray);
+	cv::threshold(gray, imgThresh, 70, 255,CV_THRESH_BINARY);
+
+	cv::Mat imgThreshCopy = imgThresh.clone();
+	//adaptiveThreshold(gray,imgThreshCopy,255,CV_ADAPTIVE_THRESH_MEAN_C, CV_THRESH_BINARY,75,10);
+
+	cv::bitwise_not(imgThreshCopy,imgThreshCopy);
+	imshow("thrash",imgThreshCopy);
+
+
+
+
+	Mat imgThresh_output;
+	//Canny( gray, imgThresh, 60, 60*3, 3 );
+	//imshow("canny",imgThresh);
+	////imgThresh = gray.clone();
+	cv::Mat structuringElement5x5 = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(3, 3));
+
+
+	for (unsigned int i = 0; i < 13; i++) {
+		cv::dilate(imgThreshCopy, imgThreshCopy, structuringElement5x5);
+		cv::dilate(imgThreshCopy, imgThreshCopy, structuringElement5x5);
+		cv::erode(imgThreshCopy, imgThreshCopy, structuringElement5x5);
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	//            cv::Mat imgThreshCopy = imgThresh.clone();
+
+	std::vector<std::vector<cv::Point> > contours;
+
+	//cv::findContours(imgThreshCopy, contours, cv::RETR_EXTERNAL,CV_CHAIN_APPROX_TC89_KCOS, Point(0, 0));
+	cv::findContours(imgThreshCopy, contours, /*cv::RETR_CCOMP CV_RETR_TREE*/ CV_RETR_LIST , cv::CHAIN_APPROX_SIMPLE);
+
+	//                                cv::drawContours(imgThreshCopy, contours, -1,cv::Scalar(255.0, 255.0, 255.0), 4);
+	//                                cv::imshow("contours", imgThreshCopy);
+	vector<vector<Point> > contours_poly( contours.size() );
+	vector<Rect> boundRect( contours.size() );
+	vector<Rect> boundRectCrop(contours.size());
+	vector<Point2f>center( contours.size() );
+	vector<float>radius( contours.size() );
+
+	for( int i = 0; i < contours.size(); i++ )
+	{ approxPolyDP( Mat(contours[i]), contours_poly[i], 3, true );
+		boundRect[i] = boundingRect( Mat(contours_poly[i]) );
+		boundRectCrop[i] = boundingRect( Mat(contours_poly[i]) );
+		minEnclosingCircle( (Mat)contours_poly[i], center[i], radius[i] );
+	}
+
+
+	//  /// Draw polygonal contour + bonding rects + circles
+	//  Mat drawing = Mat::zeros(imgThreshCopy.size(), CV_8UC3 );
+	//  for( int i = 0; i< contours.size(); i++ )
+	//     {
+	//       Scalar color = Scalar(255,123,0);
+	//       drawContours( drawing, contours_poly, i, color, 1, 8, vector<Vec4i>(), 0, Point() );
+	//       rectangle( drawing, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0 );
+	//	cout<<"p1: "<<boundRect[i].tl() << " p2: "<<boundRect[i].br()<<endl;
+	////       circle( drawing, center[i], (int)radius[i], color, 2, 8, 0 );
+	//     }
+	//cout<<endl<<endl;
+	//
+	//cv::imshow("drawings", drawing);
+
+
+
+
+
+
+
+
+
+
+	//
+	//
+	//
+	//
+	////    Pix *image = pixRead("../../helloworld.jpg");
+	//imgThresh = imgThreshCopy.clone();
+
+
+	api->TesseractRect(imgThreshCopy.data, 1, imgThreshCopy.step1(), 0, 0, imgThreshCopy.cols, imgThreshCopy.rows);
+
+
+	Pix *image = pixCreate(imgThreshCopy.size().width, imgThreshCopy.size().height, 8);
+
+	for(int i=0; i<imgThreshCopy.rows; i++) 
+		for(int j=0; j<imgThreshCopy.cols; j++) 
+			pixSetPixel(image, j,i, (l_uint32) gray.at<uchar>(i,j));
+
+	api->SetPageSegMode(static_cast<tesseract::PageSegMode>(atoi(argv[2])));
+	api->SetOutputName("out");
+
+
+
+	//api->TesseractRect(image->data, 1, image->step1(), 0, 0, image->cols, image->rows); 
+
+	api->SetImage(image);
+	api->Recognize(0);
+	//tesseract::PageIteratorLevel level1 = tesseract:: RIL_SYMBOL;
+	// Get OCR result
+	outText = api->GetUTF8Text();
+	printf("OCR output:\n%s", outText);
+
+	//float conf = 0;
+	//float conf_avg = 0;
+	int letter_count = 0;
+	int const_y1 = 0;
+	int const_y2 = 0;
+	// To crop text with counter points
+
+	tesseract::ResultIterator* ri = api->GetIterator();
+	tesseract::PageIteratorLevel level = tesseract:: RIL_SYMBOL;//RIL_WORD;
+	if (ri != 0) {
+		do {
+			const char* word = ri->GetUTF8Text(level);
+			conf = ri->Confidence(level);
+			conf_avg += conf;
+			letter_count += 1;
+			int x1, y1, x2, y2;
+			ri->BoundingBox(level, &x1, &y1, &x2, &y2);
+			if(letter_count == 1){
+				const_y1 = y1;
+				const_y2 = y2;
+			}
+
+			if(const_y2 < y2)
+				const_y2 = y2;
+
+			std::cout<<"const_y1: "<<const_y1<<" | const_y2: "<<const_y2<<std::endl;
+
+			//rectangle(imgThresh,Point(x1-20, const_y1-20),Point(x2+20, const_y2+20), Scalar(0,0,0), 2, 8, 0 );
+
+			Rect roi(x1-20, const_y1-20,(x2+20)-(x1-20),(const_y2+20)-(const_y1-20));
+
+			Mat crop_img = imgThresh(roi);
+			sprintf(imageToSave,"%s//face%d.jpg","..",letter_count);
+			imwrite(imageToSave, crop_img);
+			cv::imshow("crop", crop_img);
+
+			printf("word: '%s';  \tconf: %.2f; BoundingBox: %d,%d,%d,%d;\n",
+					word, conf, x1, y1, x2, y2);
+			delete[] word;
+		} while (ri->Next(level));
+		conf_avg = conf_avg/letter_count;
+		std::cout<<"avg conf is: "<<conf_avg<<std::endl;
+	}
+
+
+
+	/// Draw polygonal contour + bonding rects + circles
+	Mat drawing = Mat::zeros(imgThreshCopy.size(), CV_8UC3 );
+	for( int i = 0; i< contours.size(); i++ )
+	{
+		Scalar color = Scalar(255,123,0);
+		drawContours(drawing, contours_poly, i, color, 1, 8, vector<Vec4i>(), 0, Point() );
+		if((const_y1-20) <= boundRect[i].y && (const_y2+20) >= (boundRect[i].y + boundRect[i].height) && (boundRect[i].height >= (((const_y2)- (const_y1))/3))) {
+
+			cout<<"bheight: "<<boundRect[i].height << " y2-y1: "<<(const_y2)- (const_y1)<<endl;
+			rectangle(imgThreshCopy, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0 );
+			cout<<"p1: "<<boundRect[i].tl() << " p2: "<<boundRect[i].br()<<endl;
+			boundRectCrop[count_crop].x = boundRect[i].x;
+			boundRectCrop[count_crop].y = boundRect[i].y;
+			boundRectCrop[count_crop].width = boundRect[i].width;
+			boundRectCrop[count_crop].height = boundRect[i].height;
+			cout<<"p1: "<<boundRectCrop[count_crop].tl() << " p2: "<<boundRectCrop[count_crop].br()<<endl;
+			count_crop += 1;
+
+			//Rect roi_crop(boundRect[i].x, boundRect[i].y,boundRect[i].width,boundRect[i].height);
+			//
+			//Mat crop_img_text = imgThresh(roi_crop);
+			//                sprintf(imageToSave_crop,"%s//crop%d.jpg","..",i);
+			//                imwrite(imageToSave_crop, crop_img_text);
+			//cv::imshow("crop_text", crop_img_text);
+
+		}
+		//       circle( drawing, center[i], (int)radius[i], color, 2, 8, 0 );
+	}
+
+
+	sort(boundRectCrop);
+	cout<<endl<<endl;
+	for( int i = 0; i< count_crop; i++ )
+	{
+		cout<<"p1: "<<boundRectCrop[i].tl() << " p2: "<<boundRectCrop[i].br()<<endl;
+		Rect roi_crop(boundRectCrop[i].x, boundRectCrop[i].y,boundRectCrop[i].width,boundRectCrop[i].height);
+
+		Mat crop_img_text = imgThresh(roi_crop);
+		sprintf(imageToSave_crop,"%s//crop%d.jpg","../crop/",i);
+		imwrite(imageToSave_crop, crop_img_text);
+		cv::imshow("crop_text", crop_img_text);
+
+	}
+
+	cout<<endl<<endl;
+	cv::imshow("contours_rect", imgThreshCopy);
+	cv::imshow("drawings", drawing);
+
+
+
+
+
+
+
+
+
+
+	//}while(conf_avg < 90);
+
+	cv::imshow("rect", imgThresh);
+	waitKey(0);
+
+	// Destroy used object and release memory
+	//    api->End();
+	//    delete [] outText;
+	//    pixDestroy(&image);
+
+	//    return 0;
 }
 
-//api->SetVariable("segment_penalty_garbage", "0");
-//api->SetVariable("segment_penalty_dict_nonword", "0");
-//api->SetVariable("segment_penalty_dict_frequent_word", "0");
-//api->SetVariable("segment_penalty_dict_case_ok", "0");
-//api->SetVariable("segment_penalty_dict_case_bad", "0");
-// api->SetPageSegMode(static_cast<tesseract::PageSegMode>(7));
-// api->SetOutputName("out");
 
-    // Initialize tesseract-ocr with English, without specifying tessdata path
-//    if (api->Init(NULL, "eng")) {
-//        fprintf(stderr, "Could not initialize tesseract.\n");
-//        exit(1);
-//    }
 
+void sort(vector<Rect> &boundRectCrop){
 
 
-    // Open input image with leptonica library
-//    Pix *image = pixRead("../../helloworld.jpg");
-//
-// api->SetPageSegMode(static_cast<tesseract::PageSegMode>(7));
-// api->SetOutputName("out");
+	for(int i = 0; i< count_crop; i++){
+		for(int j = i; j< count_crop; j++){
+			if(boundRectCrop[i].x > boundRectCrop[j].x){
 
-Mat gray, imgThresh;
-Mat img_src = imread(argv[1]);
-float conf = 0;
-float conf_avg = 0;
-//do{
-cvtColor(img_src,gray,CV_BGR2GRAY);
-imshow("gray",gray);
-cv::GaussianBlur(gray, gray, cv::Size(3, 3), 0);
-imshow("blur",gray);
-cv::threshold(gray, imgThresh, 70, 255,CV_THRESH_BINARY);
+				int tempX = boundRectCrop[i].x;
+				int tempY = boundRectCrop[i].y;
+				int tempWidth = boundRectCrop[i].width;
+				int tempHeight = boundRectCrop[i].height;
 
-cv::Mat imgThreshCopy = imgThresh.clone();
-//adaptiveThreshold(gray,imgThreshCopy,255,CV_ADAPTIVE_THRESH_MEAN_C, CV_THRESH_BINARY,75,10);
+				boundRectCrop[i].x = boundRectCrop[j].x;
+				boundRectCrop[i].y = boundRectCrop[j].y;
+				boundRectCrop[i].width = boundRectCrop[j].width;
+				boundRectCrop[i].height = boundRectCrop[j].height;
 
-cv::bitwise_not(imgThreshCopy,imgThreshCopy);
-imshow("thrash",imgThreshCopy);
+				boundRectCrop[j].x = tempX;
+				boundRectCrop[j].y = tempY;
+				boundRectCrop[j].width = tempWidth;
+				boundRectCrop[j].height = tempHeight;
 
+			}
 
+		}
+		cout<<"p1: "<<boundRectCrop[i].tl() << " p2: "<<boundRectCrop[i].br()<<endl;
+	}
 
-
-Mat imgThresh_output;
-//Canny( gray, imgThresh, 60, 60*3, 3 );
-//imshow("canny",imgThresh);
-////imgThresh = gray.clone();
-                                cv::Mat structuringElement5x5 = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(3, 3));
-
-
-                                for (unsigned int i = 0; i < 13; i++) {
-                                        cv::dilate(imgThreshCopy, imgThreshCopy, structuringElement5x5);
-                                        cv::dilate(imgThreshCopy, imgThreshCopy, structuringElement5x5);
-                                        cv::erode(imgThreshCopy, imgThreshCopy, structuringElement5x5);
-                                }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                    //            cv::Mat imgThreshCopy = imgThresh.clone();
-
-                                std::vector<std::vector<cv::Point> > contours;
-
-                                //cv::findContours(imgThreshCopy, contours, cv::RETR_EXTERNAL,CV_CHAIN_APPROX_TC89_KCOS, Point(0, 0));
-				cv::findContours(imgThreshCopy, contours, cv::RETR_CCOMP /*CV_RETR_TREE*/ , cv::CHAIN_APPROX_SIMPLE);
-
-//                                cv::drawContours(imgThreshCopy, contours, -1,cv::Scalar(255.0, 255.0, 255.0), 4);
-//                                cv::imshow("contours", imgThreshCopy);
-  vector<vector<Point> > contours_poly( contours.size() );
-  vector<Rect> boundRect( contours.size() );
-vector<Rect> boundRectCrop(contours.size());
-  vector<Point2f>center( contours.size() );
-  vector<float>radius( contours.size() );
-
-  for( int i = 0; i < contours.size(); i++ )
-     { approxPolyDP( Mat(contours[i]), contours_poly[i], 3, true );
-       boundRect[i] = boundingRect( Mat(contours_poly[i]) );
-       minEnclosingCircle( (Mat)contours_poly[i], center[i], radius[i] );
-     }
-
-
-//  /// Draw polygonal contour + bonding rects + circles
-//  Mat drawing = Mat::zeros(imgThreshCopy.size(), CV_8UC3 );
-//  for( int i = 0; i< contours.size(); i++ )
-//     {
-//       Scalar color = Scalar(255,123,0);
-//       drawContours( drawing, contours_poly, i, color, 1, 8, vector<Vec4i>(), 0, Point() );
-//       rectangle( drawing, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0 );
-//	cout<<"p1: "<<boundRect[i].tl() << " p2: "<<boundRect[i].br()<<endl;
-////       circle( drawing, center[i], (int)radius[i], color, 2, 8, 0 );
-//     }
-//cout<<endl<<endl;
-//
-//cv::imshow("drawings", drawing);
-
-
-
-
-
-
-
-
-
-
-//
-//
-//
-//
-////    Pix *image = pixRead("../../helloworld.jpg");
-//imgThresh = imgThreshCopy.clone();
-
-
-api->TesseractRect(imgThreshCopy.data, 1, imgThreshCopy.step1(), 0, 0, imgThreshCopy.cols, imgThreshCopy.rows);
-
-
-Pix *image = pixCreate(imgThreshCopy.size().width, imgThreshCopy.size().height, 8);
-
-for(int i=0; i<imgThreshCopy.rows; i++) 
-    for(int j=0; j<imgThreshCopy.cols; j++) 
-        pixSetPixel(image, j,i, (l_uint32) gray.at<uchar>(i,j));
-
- api->SetPageSegMode(static_cast<tesseract::PageSegMode>(atoi(argv[2])));
- api->SetOutputName("out");
-
-
-
-//api->TesseractRect(image->data, 1, image->step1(), 0, 0, image->cols, image->rows); 
- 
-   api->SetImage(image);
-api->Recognize(0);
-//tesseract::PageIteratorLevel level1 = tesseract:: RIL_SYMBOL;
-    // Get OCR result
-    outText = api->GetUTF8Text();
-    printf("OCR output:\n%s", outText);
-
-//float conf = 0;
-//float conf_avg = 0;
-int letter_count = 0;
-int const_y1 = 0;
-int const_y2 = 0;
-// To crop text with counter points
-
-tesseract::ResultIterator* ri = api->GetIterator();
-  tesseract::PageIteratorLevel level = tesseract:: RIL_SYMBOL;//RIL_WORD;
-  if (ri != 0) {
-    do {
-      const char* word = ri->GetUTF8Text(level);
-      conf = ri->Confidence(level);
-conf_avg += conf;
-letter_count += 1;
-      int x1, y1, x2, y2;
-      ri->BoundingBox(level, &x1, &y1, &x2, &y2);
-if(letter_count == 1){
-   const_y1 = y1;
-const_y2 = y2;
-}
-
-if(const_y2 < y2)
-const_y2 = y2;
-
-std::cout<<"const_y1: "<<const_y1<<" | const_y2: "<<const_y2<<std::endl;
-
-//rectangle(imgThresh,Point(x1-20, const_y1-20),Point(x2+20, const_y2+20), Scalar(0,0,0), 2, 8, 0 );
-
-Rect roi(x1-20, const_y1-20,(x2+20)-(x1-20),(const_y2+20)-(const_y1-20));
-
-Mat crop_img = imgThresh(roi);
-                sprintf(imageToSave,"%s//face%d.jpg","..",letter_count);
-                imwrite(imageToSave, crop_img);
-cv::imshow("crop", crop_img);
-
-      printf("word: '%s';  \tconf: %.2f; BoundingBox: %d,%d,%d,%d;\n",
-               word, conf, x1, y1, x2, y2);
-      delete[] word;
-    } while (ri->Next(level));
-conf_avg = conf_avg/letter_count;
-std::cout<<"avg conf is: "<<conf_avg<<std::endl;
-  }
-
-
-
-  /// Draw polygonal contour + bonding rects + circles
-Mat drawing = Mat::zeros(imgThreshCopy.size(), CV_8UC3 );
-  for( int i = 0; i< contours.size(); i++ )
-     {
-       Scalar color = Scalar(255,123,0);
-       drawContours(drawing, contours_poly, i, color, 1, 8, vector<Vec4i>(), 0, Point() );
-        if((const_y1-20) <= boundRect[i].y && (const_y2+20) >= (boundRect[i].y + boundRect[i].height) && (boundRect[i].height >= ((const_y2)- (const_y1))/2) ){
-        cout<<"bheight: "<<boundRect[i].height << " y2-y1: "<<(const_y2)- (const_y1)<<endl;
-       rectangle(imgThreshCopy, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0 );
-        cout<<"p1: "<<boundRect[i].tl() << " p2: "<<boundRect[i].br()<<endl;
-//        boundRectCrop[i].tl() = boundRect[i].tl();
-//        boundRectCrop[i].br() = boundRect[i].br();
-Rect roi_crop(boundRect[i].x, boundRect[i].y,boundRect[i].width,boundRect[i].height);
-
-Mat crop_img_text = imgThresh(roi_crop);
-                sprintf(imageToSave_crop,"%s//crop%d.jpg","..",i);
-                imwrite(imageToSave_crop, crop_img_text);
-cv::imshow("crop_text", crop_img_text);
-
-
-
-
-
-
-
-}
-//       circle( drawing, center[i], (int)radius[i], color, 2, 8, 0 );
-     }
-cout<<endl<<endl;
-cv::imshow("contours_rect", imgThreshCopy);
-cv::imshow("drawings", drawing);
-
-
-
-
-
-
-
-
-
-
-//}while(conf_avg < 90);
-
-cv::imshow("rect", imgThresh);
-waitKey(0);
-
-    // Destroy used object and release memory
-//    api->End();
-//    delete [] outText;
-//    pixDestroy(&image);
-
-//    return 0;
 }
--- module2/sample1.cpp
+++ module2/sample1.cpp
@@ -2,6 +2,7 @@
 #include <leptonica/allheaders.h>
 #include <iostream>
 #include <string.h>
+#include <sstream>
 #include <vector>
 #include <opencv2/highgui/highgui.hpp>
 #include "opencv2/imgproc/imgproc.hpp"
@@ -12,52 +13,88 @@ using namespace cv;
 
 void read_image(int width, int height, char *image);
 Mat scene_plate;
+string lpr = "";
+int count_letters = 0;
+int count_non_letters = 0;
 
 int main( int argc, char** argv )
 {
+	VideoCapture cap("/home/rohit/Desktop/source_ocr/module1/crop/crop%d.jpg"); // would work with: "/my/folder/p00013.jpg", etc
+	cout << "OCR: starts" << endl;
+	while(cap.read(scene_plate)){
 
-  cout << "OCR: starts" << endl;
-  scene_plate = imread(argv[1], CV_LOAD_IMAGE_COLOR );
-  read_image(scene_plate.cols, scene_plate.rows, (char*)scene_plate.ptr());
+		//cap.read(scene_plate);
+		//  cout << "OCR: starts" << endl;
+		//scene_plate = imread(argv[1], CV_LOAD_IMAGE_COLOR );
+		read_image(scene_plate.cols, scene_plate.rows, (char*)scene_plate.ptr());
+	}
+	lpr.erase(std::remove(lpr.begin(), lpr.end(), '\n'), lpr.end());
+	lpr.erase(std::remove(lpr.begin(), lpr.end(), ' '), lpr.end());
+	cout<<lpr<<endl;
 }
 
 
- void read_image(int width, int height, char *image)
+void read_image(int width, int height, char *image)
 
- {
-    cv::Mat Image(height, width, CV_8UC3, image);
-    // you may need to define the area of interest, where the test is found
-
-// initializing Tesseract API
- char *outText;
-double confidence = 0;
-do{
-tesseract::TessBaseAPI *tess_api = new tesseract::TessBaseAPI();
-if (tess_api->Init(NULL, "eng"))  // eng is a flag of which trained language you use, if you just train your own language, you gave "XYZ" as a falge, you have to use it here
-   {
-    cout<<"Could not initialize tesseract.\n";
-    exit(1);
-}
-//tess_api->SetVariable("tessedit_char_whitelist", "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz012345789");
-//tess_api->SetVariable("tessedit_char_whitelist", "0123456789");
-tess_api->SetVariable("tessedit_char_whitelist", /*"ABCDEFGHIJKLMNOPQRSTUVWXYZ*/"0123456789");
-tess_api->SetVariable("classify_font_name", "Arial.ttf");
-tess_api->SetVariable("segment_penalty_garbage", "0");
-tess_api->SetVariable("segment_penalty_dict_nonword", "0");
-tess_api->SetVariable("segment_penalty_dict_frequent_word", "0");
-tess_api->SetVariable("segment_penalty_dict_case_ok", "0");
-tess_api->SetVariable("segment_penalty_dict_case_bad", "0");
-
-
-if(!tess_api->SetVariable("tessedit_enable_doc_dict", "0"))
 {
-    cout << "Unable to enable dictionary" << endl;
+	cv::Mat Image(height, width, CV_8UC3, image);
+	// you may need to define the area of interest, where the test is found
+
+	// initializing Tesseract API
+	char *outText;
+	double confidence = 0;
+	//do{
+	tesseract::TessBaseAPI *tess_api = new tesseract::TessBaseAPI();
+	if (tess_api->Init(NULL, "eng"))  // eng is a flag of which trained language you use, if you just train your own language, you gave "XYZ" as a falge, you have to use it here
+	{
+		cout<<"Could not initialize tesseract.\n";
+		exit(1);
+	}
+	//tess_api->SetVariable("tessedit_char_whitelist", "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz012345789");
+	//tess_api->SetVariable("tessedit_char_whitelist", "0123456789");
+//	if(count_letters <= 4)
+//	tess_api->SetVariable("tessedit_char_whitelist", "ABCDEFGHIJKLMNOPQRSTUVWXYZ"/*0123456789X"*/);
+//	if(count_letters >=4 && count_letters <=12)
+//	tess_api->SetVariable("tessedit_char_whitelist", /*"ABCDEFGHIJKLMNOPQRSTUVWXYZ"*/"0123456789");
+//	if(count_letters>=12  && count_letters < 18)
+//	tess_api->SetVariable("tessedit_char_whitelist", "ABCDEFGHIJKLMNOPQRSTUVWXYZ"/*0123456789X"*/);
+//	if(count_letters>=18)
+        tess_api->SetVariable("tessedit_char_whitelist", "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789");
+//	else
+//	tess_api->SetVariable("tessedit_char_whitelist", "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789X");
+
+	tess_api->SetVariable("classify_font_name", "Arial.ttf");
+	tess_api->SetVariable("segment_penalty_garbage", "0");
+	tess_api->SetVariable("segment_penalty_dict_nonword", "0");
+	tess_api->SetVariable("segment_penalty_dict_frequent_word", "0");
+	tess_api->SetVariable("segment_penalty_dict_case_ok", "0");
+	tess_api->SetVariable("segment_penalty_dict_case_bad", "0");
+
+
+	if(!tess_api->SetVariable("tessedit_enable_doc_dict", "0"))
+	{
+		cout << "Unable to enable dictionary" << endl;
+	}
+	tess_api->SetPageSegMode(tesseract::PSM_SINGLE_WORD);
+	tess_api->SetImage((uchar*)scene_plate.data, scene_plate.size().width,scene_plate.size().height,scene_plate.channels(),scene_plate.step1());
+	tess_api->Recognize(0);
+	//char* out =tess_api->GetUTF8Text();
+	string temp = tess_api->GetUTF8Text();
+	confidence = tess_api->MeanTextConf();
+	//cout<<"OCR output:"<< out<< "  with confidence "<<confidence<<endl;
+	//lpr << out; 
+	//cout<<temp.length()<<endl;
+
+	if(temp.length() <= 5){
+		lpr +=temp;
+		count_letters +=temp.length();
+		cout<<count_letters<<" | "<<lpr<<endl;
+	}
+	else{
+		count_non_letters +=temp.length();
+	}
+	//lpr.append(out);
+	//cout<<lpr;
+
+	//}while(confidence < 90);
 }
-tess_api->SetPageSegMode(tesseract::PSM_SINGLE_CHAR);
-tess_api->SetImage((uchar*)scene_plate.data, scene_plate.size().width,scene_plate.size().height,scene_plate.channels(),scene_plate.step1());
-tess_api->Recognize(0);
-char* out =tess_api->GetUTF8Text();
-confidence = tess_api->MeanTextConf();
-cout<<"OCR output:"<< out<< "  with confidence "<<confidence<<endl;
-}while(confidence < 90);
- }
